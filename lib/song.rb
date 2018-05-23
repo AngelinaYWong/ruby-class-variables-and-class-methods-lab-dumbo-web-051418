aae0288070d4c306e6db10c1e1f22b94717bab95
Angelina Wong <angelina.w25@gmail.com>
2018-05-23 23:13:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,13 +29,19 @@ class Song
   end
   
   def self.genre_count
-    
-    
+    genre_count = Hash.new(0)
+    @@genres.each do |genre|
+      genre_count[genre] += 1
+    end
+    return genre_count
   end
-  
+
   def self.artist_count
-    
-    
+    artist_count = Hash.new(0)
+    @@artists.each do |artist|
+      artist_count[artist] += 1
+    end
+    return artist_count
   end
   
 end
